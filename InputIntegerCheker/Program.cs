d12fd0b5c42bdf4327146d755eef228ba1e44c3b
Rioreyblue <franciscorey702@gmail.com>
2026-06-05 23:45:04 +0800

    add logic for input checker

--- InputIntegerCheker/Program.cs
+++ InputIntegerCheker/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //global variables
+        int new_user_input
+        
+        //user input
+        Console.WriteLine("Enter integer input: ");
+        string user_input = Console.ReadLine();
+        
+        //condition
+        if(!int.TryParse(user_input, out new_user_input)){
+            Console.WriteLine("Invalid input! Please select only Integer input.");
+        }else{
+            Console.WriteLine($"Exellent choice: {new_user_input}");
+        }
