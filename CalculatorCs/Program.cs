fcd8afc4e24b4717e0be56f71e1ddf47eb7d28ad
Rioreyblue <franciscorey702@gmail.com>
2026-06-06 23:43:54 +0800

    done user input for user

--- CalculatorCs/Program.cs
+++ CalculatorCs/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Enter a first number: ");
+int num1 = Int.Parse(Console.ReadLine());
+
+Console.Write("Enter an operator: ");
+string operator = Console.ReadLine();
+
+Console.Write("Enter a second number: ");
+int num2 = Int.Parse(Console.ReadLine());
