2589c99731e916cc0b066c042136fc26c08136df
Rioreyblue <franciscorey702@gmail.com>
2026-06-06 23:59:39 +0800

    add addition

--- CalculatorCs/Program.cs
+++ CalculatorCs/Program.cs
@@ -1,8 +1,33 @@
-﻿Console.Write("Enter a first number: ");
-int num1 = Int.Parse(Console.ReadLine());
+﻿//user input
+
+Console.Write("Enter a first number: ");
+int num1 = int.Parse(Console.ReadLine());
 
 Console.Write("Enter an operator: ");
-string operator = Console.ReadLine();
+Console.WriteLine("""Please select an operator in the following:
+{+}, {-}, {x}, {/}""");
+string op = Console.ReadLine().ToLower();
 
 Console.Write("Enter a second number: ");
-int num2 = Int.Parse(Console.ReadLine());
+int num2 = int.Parse(Console.ReadLine());
+
+//all op(operator)
+double totalSum = 0;
+double totalSub = 0;
+double totalMul = 0;
+double totalDiv = 0;
+
+//op formula
+
+if(op == "+"){
+    totalSum = num1 + num2;
+    Console.WriteLine($"The total Sum is: {totalSum}");
+}
+
+
+
+
+//function
+// private static string OperatorMsg(){
+//     return "x";
+// }
