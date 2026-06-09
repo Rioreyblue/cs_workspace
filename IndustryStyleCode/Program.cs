a00f558ad295d48fdd0db2c8dafa4ca669c83970
Rioreyblue <franciscorey702@gmail.com>
2026-06-09 23:56:34 +0800

    add a geter first and a function for user input with restriction

--- IndustryStyleCode/Program.cs
+++ IndustryStyleCode/Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿//function for main
+//geter function
+static void Main(){
+    //safe number input for 1
+    int input_1 = getValidInteger("Enter a first number: ");
+
+    //safe operator input
+    string op = getValidOperator();
+
+    //safe number input for 2
+    int input_2 = getValidInteger("Enter a first number: ");
+}
+
+//input function
+private static int getValidInteger(string prompt){
+
+
+}
