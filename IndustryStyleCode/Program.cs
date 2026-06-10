d966f761160131b3785ab909543fae521c74c5d7
Rioreyblue <franciscorey702@gmail.com>
2026-06-10 23:14:47 +0800

    input function filtering

--- IndustryStyleCode/Program.cs
+++ IndustryStyleCode/Program.cs
@@ -5,7 +5,7 @@ static void Main(){
     int input_1 = getValidInteger("Enter a first number: ");
 
     //safe operator input
-    string op = getValidOperator();
+    // string op = getValidOperator();
 
     //safe number input for 2
     int input_2 = getValidInteger("Enter a first number: ");
@@ -13,6 +13,13 @@ static void Main(){
 
 //input function
 private static int getValidInteger(string prompt){
+    int valid_input;
+    Console.Write(prompt);
 
-
+    Console.WriteLine(" Enter a first number: ")
+    if(!int.TryParse(Console.Readline(), out valid_input)){
+        Console.WriteLine("Invalid input, only put numbers: ");
+        Console.Write(prompt);
+    }
+    return valid_input;
 }
