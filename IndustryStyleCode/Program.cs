be420ae92b61ac3f0371fa3a45fa41f230367274
Rioreyblue <franciscorey702@gmail.com>
2026-06-10 23:41:59 +0800

    debugging

--- IndustryStyleCode/Program.cs
+++ IndustryStyleCode/Program.cs
@@ -1,27 +1,37 @@
-﻿//function for main
-//geter function
-static void Main(){
-    //safe number input for 1
-    int input_1 = getValidInteger("Enter a first number: ");
+﻿using System;
 
-    //safe operator input
-    // string op = getValidOperator();
-
-    //safe number input for 2
-    int input_2 = getValidInteger("Enter a second number: ");
-}
-
-//input function
-private static int getValidInteger(string prompt){
-    int valid_input;
-    Console.Write(prompt);
-
-    while (!int.TryParse(Console.ReadLine(), out valid_input))
+class Program
+{
+    //function for main
+    //geter function
+    static void Main()
     {
-        Console.ForegroundColor = ConsoleColor.Blue;
-        Console.WriteLine("Invalid input, only put numbers: ");
-        Console.ResetColor();
-        Console.Write(prompt);
+        //safe number input for 1
+        int input_1 = getValidInteger("Enter a first number: ");
+
+        //safe operator input
+        // string op = getValidOperator();
+
+        //safe number input for 2
+        int input_2 = getValidInteger("Enter a second number: ");
     }
-    return valid_input;
+
+    //input function
+    private static int getValidInteger(string prompt)
+    {
+        int valid_input;
+        Console.Write(prompt);
+
+        while (!int.TryParse(Console.ReadLine().ToLower(), out valid_input))
+        {
+            if(valid_input == "exit")
+            break;
+            Console.ForegroundColor = ConsoleColor.Red;
+            //restriction
+            Console.WriteLine("Invalid input, only put numbers only!);
+            Console.ResetColor();
+            Console.Write(prompt);
+        }
+        return valid_input;
+    }
 }
