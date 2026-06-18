f04d024d6cc6accf7328dc5fca032ce3a9852784
Rioreyblue <franciscorey702@gmail.com>
2026-06-18 23:48:39 +0800

    add an invalid reasoning for entering name

--- PassCodeSystem/Program.cs
+++ PassCodeSystem/Program.cs
@@ -13,7 +13,19 @@ class Program
     }
 
     //function input
-    private static string getValidString() { }
+    private static string getValidString(string input) {
+        //for declaration
+        string valid_input = "";
+
+        
+
+        if()
+
+        //invalid reasoning
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("Invalid input. Please enter letter for name");
+        Console.ResetColor();
+     }
 
     private static int getValidCode(){
         
