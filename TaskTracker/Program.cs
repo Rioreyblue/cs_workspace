d6163687357e776b5c3bcaab7227c94d6ee2020b
Rioreyblue <franciscorey702@gmail.com>
2026-06-24 23:35:50 +0800

    core function methods

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -1,5 +1,15 @@
 ﻿using System;
 
 class Program{
-    
+    static void Main(){
+        //geter
+    }
+}
+
+static void HandleAdd(string[] args){
+    //add logic
+}
+
+static void HandleList(){
+    //handle list logic
 }
