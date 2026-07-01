080cc489ba4907150bd8a547cf95db745cc60ff8
Rioreyblue <franciscorey702@gmail.com>
2026-07-02 00:00:07 +0800

    final part of the code for thew main program of Guessing game

--- GuessingGame/Program.cs
+++ GuessingGame/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using GuessingGame.Src;
+
+while (true)
+{
+    GameLogic.PlayGame();
+
+    Console.Write("\nDo you want to play again? (y/n): ");
+    string playAgain = Console.ReadLine()?.ToLower().Trim() ?? string.Empty;
+
+    if (playAgain != "y")
+    {
+        Console.WriteLine("Thanks for playing! Goodbye.");
+        break;
+    }
+}
